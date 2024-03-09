42917b572488d6660d00f8529456b731b334c99f
readysloth <readysloth@protonmail.com>
2024-03-09 13:45:56 +0300

    add(telegram_bot/ext.py): Added functions for simple broadcasting

---
+++ django_telegram_bot/telegram_bot/ext.py
@@ -0,0 +1,47 @@
+import logging
+
+from typing import Callable
+
+from django_telegram_bot.models import User
+
+from telegram.ext import Bot
+from telegram.constants import ParseMode
+
+from asgiref.sync import sync_to_async
+
+from .bot import oneshot_task, periodic_task
+
+UserPredicate = Callable[[User], bool]
+
+
+def simple_broadcast_task(message: str,
+                          user_predicate: UserPredicate = lambda *args, **kwargs: True):
+    @oneshot_task
+    async def task(bot: Bot):
+        async for user in sync_to_async(User.objects.all)():
+            try:
+                if user_predicate(user):
+                    await bot.send_message(user.telegram_id,
+                                           message,
+                                           parse_mode=ParseMode.HTML)
+            except Exception:
+                logging.exception(f'Attempt to send "{message}" to {user.telegram_id} failed')
+                continue
+    return task
+
+
+def simple_periodic_task(sleep_time: int,
+                         message: str,
+                         user_predicate: UserPredicate = lambda *args, **kwargs: True):
+    @periodic_task(sleep_time)
+    async def task(bot: Bot):
+        async for user in sync_to_async(User.objects.all)():
+            try:
+                if user_predicate(user):
+                    await bot.send_message(user.telegram_id,
+                                           message,
+                                           parse_mode=ParseMode.HTML)
+            except Exception:
+                logging.exception(f'Attempt to send "{message}" to {user.telegram_id} failed')
+                continue
+    return task
